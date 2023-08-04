737b27252d0c96c71bf7b8546c4db0376a32ad10
Philipp Benner <philipp.benner@bam.de>
2023-08-04 20:19:30 +0200

    Updated alignn-ff model training

--- models/alignn/train_alignn_ff.sh
+++ models/alignn/train_alignn_ff.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-python ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json > train_alignn_ff.log 2>&1 & disown
-
+python ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown

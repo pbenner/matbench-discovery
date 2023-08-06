ed629565975df81e2f421510c30e8f94bac3730a
Philipp Benner <philipp.benner@bam.de>
2023-08-06 09:19:00 +0200

    Bugfix in ALIGNN-FF patch

--- models/alignn/train_alignn_ff.sh
+++ models/alignn/train_alignn_ff.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 python ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown
+
+accelerate launch --multi_gpu ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown

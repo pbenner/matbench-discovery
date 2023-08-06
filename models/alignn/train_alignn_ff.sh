4beeefd65d6ea10a528529a69c77246f4b863697
Philipp Benner <philipp.benner@bam.de>
2023-08-06 16:37:33 +0200

    Removed multi-gpu mode from script

--- models/alignn/train_alignn_ff.sh
+++ models/alignn/train_alignn_ff.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-python ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown
+# Multi-GPU mode blows main memory, because each process gets a complete copy of the training data
+# accelerate launch --multi_gpu ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown
 
-accelerate launch --multi_gpu ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown
+python ~/Source/tmp/alignn/alignn/train_folder_ff.py --root_dir data-train-ff --output_dir data-train-ff-result --config_name alignn-ff-config.json --restart_model_path ../../../alignn/alignn/ff/alignnff_wt10/best_model.pt > train_alignn_ff.log 2>&1 & disown

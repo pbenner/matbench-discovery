e5396e9e7e9414f755de03662e27d80e698b7c7c
Philipp Benner <philipp.benner@bam.de>
2023-09-25 10:18:12 +0200

    2023/09/25-10:18:12 (Linux sv2213 unknown)

--- models/mace/train_mace.sh
+++ models/mace/train_mace.sh
@@ -5,7 +5,7 @@ OUTDIR=data-train-result
 
 mkdir -p ${data-train-result}
 
-python ./mace/scripts/run_train.py \
+accelerate launch --multi_gpu --num_processes=4 ./mace/scripts/run_train.py \
     --name="MACE_MPtrj_2022.9" \
     --log_dir="${OUTDIR}/logs" \
     --model_dir="${OUTDIR}" \
@@ -15,20 +15,22 @@ python ./mace/scripts/run_train.py \
     --train_file="./${BASEDIR}/train.h5" \
     --valid_file="./${BASEDIR}/valid.h5" \
     --statistics_file="./${BASEDIR}/statistics.json" \
-    --num_workers=16 \
+    --num_workers=8 \
     --model="ScaleShiftMACE" \
     --num_interactions=2 \
-    --num_channels=128 \
+    --num_channels=256 \
     --max_L=1 \
     --correlation=3 \
     --batch_size=32 \
     --valid_batch_size=32 \
     --max_num_epochs=100 \
-    --swa \
-    --start_swa=60 \
     --ema \
     --ema_decay=0.99 \
     --amsgrad \
     --error_table='PerAtomMAE' \
-    --device=cuda \
-    --seed=42
+    --seed=42 \
+    --compute_stress True \
+    --forces_weight=1.0 \
+    --energy_weight=1.0 \
+    --stress_weight=0.5 \
+    --restart_latest

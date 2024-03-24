818173c68f434b02d4139c7fb032bd703af83fe9
Philipp Benner <philipp.benner@bam.de>
2024-03-24 11:13:08 +0100

    reduced batch size due to out of memory error

--- models/equiformer_v2/train_equiformer.py
+++ models/equiformer_v2/train_equiformer.py
@@ -10,10 +10,16 @@ def main():
     r = 4.5
 
     args = get_args_parser().parse_args()
-    args.train_file = f'data-r{r}/train.h5'
-    args.valid_file = f'data-r{r}/valid.h5'
+
+    args.train_file      = f'data-r{r}/train.h5'
+    args.valid_file      = f'data-r{r}/valid.h5'
     args.statistics_file = f'data-r{r}/statistics.json'
-    args.output_dir = 'result'
+    args.output_dir      = 'result'
+
+    args.epochs     = 100
+    args.batch_size = 5
+    args.lr         = 0.001
+    args.warmup_lr  = 0.01
 
     train(args)
 
